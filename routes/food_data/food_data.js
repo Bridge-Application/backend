1cb7e3a163e9ed85812df2ecfd669f112e37e4d3
Isaac Tong <isaact943@outlook.com>
2020-08-15 20:42:08 +0800

    Filtered out all data except if data is a survey

--- routes/food_data/food_data.js
+++ routes/food_data/food_data.js
@@ -4,13 +4,21 @@ const axios = require('axios')
 require('dotenv').config()
 
 router.get('/', async (req, res) => {
-    const response = await axios.get('https://api.nal.usda.gov/fdc/v1/foods/search', {
+    try {
+        const response = await axios.get('https://api.nal.usda.gov/fdc/v1/foods/search', {
             
-    headers: {'X-Api-Key': process.env.FDC_API_KEY},
-    params: {'query': req.body.query}
-    });
-
-    res.send(response.data.foods)
+            headers: {'X-Api-Key': process.env.FDC_API_KEY},
+            params: {
+                'query': req.body.query,
+                'dataType': 'Survey (FNDDS)'
+            }
+            });
+            console.log('Success')
+            res.send(response.data.foods)
+    } catch (error) {
+        res.send(error);
+    }
+   
 });
 
 module.exports = router;
